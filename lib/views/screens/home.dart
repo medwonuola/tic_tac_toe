2946ac9684b77c48c0ee3d7ed14060715883eba1
Arigbabu Abdul-Ahmed <Medwonuola@gmail.com>
2021-06-28 11:39:01 +0100

    Initial commit

---
+++ lib/views/screens/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text("Start Timer"),
+        icon: Icon(Icons.av_timer),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }}
